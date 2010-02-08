a6c6354ad033c8a9cba9c3591a3ffe75cee18e80
Dmitry Antonyuk <lomeo.nuke@gmail.com>
2010-02-08 16:07:06 +0300

    Added test with different types.

--- test/Test.hs
+++ test/Test.hs
@@ -13,10 +13,21 @@ roses =
         c = Rose 3 [a,b]
     in a
 
+data A = A B
+    deriving (Data,Typeable)
+data B = B A
+    deriving (Data,Typeable)
+
 recLimit_prop = length [ r | r@Rose{} <- universeRec roses ] == 3
 recOrder_prop = [ i | Rose i _ <- universeRec roses] == [1,2,3]
+
+recAB_prop = length [ b | b@B{} <- universeRec a ] == 1
+    where
+        a = A b
+        b = B a
 
 main = do
     quickCheck recLimit_prop
     quickCheck recOrder_prop
+    quickCheck recAB_prop
     return ()
